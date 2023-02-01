eb50382e9dfeee34b5cd5fd6688c0182683e580e
thesaddest <artsemshauchuk@gmail.com>
2023-02-01 13:18:16 +0100

    add: styles init, theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import "./index.scss";
+import "./styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main/MainPage";
 import { AboutPageAsync } from "./pages/about/AboutPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={"/"}>Main</Link>
             <Link to={"/about"}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                        <Route path={"/about"} element={<AboutPageAsync />}/>
-                    <Route path={"/"} element={<MainPage />}/>
+                    <Route path={"/about"} element={<AboutPageAsync />} />
+                    <Route path={"/"} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>

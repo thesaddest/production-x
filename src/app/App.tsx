e47342dd35ed54c870fdff3067000fc4a19540fd
thesaddest <artsemshauchuk@gmail.com>
2023-02-26 16:05:02 +0100

    feat: error boundary, fix: minor fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,14 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/navbar";
 import { Sidebar } from "widgets/sidebar";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 const App = () => {
     const { theme } = useTheme();
 
+    useEffect(() => {
+        throw new Error();
+    }, []);
     return (
         <div className={classNames("app", {}, [theme])}>
             <Suspense fallback="">
---
+++ src/widgets/page-error/ui/PageError.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./PageError.module.scss";
+import { useTranslation } from "react-i18next";
+
+interface PageErrorProps {
+    className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+    const { t } = useTranslation();
+
+    const reloadPage = () => {
+        location.reload();
+    };
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>{t("UNEXPECTED ERROR")}</p>
+            <button onClick={reloadPage}>{t("REFRESH THE PAGE")}</button>
+        </div>
+    );
+};

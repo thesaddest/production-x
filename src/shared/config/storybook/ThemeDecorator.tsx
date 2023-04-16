d78c0b536fd7fb918aebdaf71fefb5276b0bb782
thesaddest <artsemshauchuk@gmail.com>
2023-04-16 16:08:22 +0200

    fix: display-name rule in storybok ThemeDecorator

--- src/shared/config/storybook/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator.tsx
@@ -2,10 +2,14 @@ import "app/styles/index.scss";
 import { StoryFn } from "@storybook/react";
 import { Theme } from "app/providers/ThemeProvider";
 
-export const ThemeDecorator = (storyTheme: Theme) => (StoryComponent: StoryFn) => {
-    return (
-        <div className={`app ${storyTheme}`}>
-            <StoryComponent />
-        </div>
-    );
+export const ThemeDecorator = (StoryComponent: StoryFn) => {
+    const HOCStoryComponent = (storyTheme: Theme) => {
+        return (
+            <div className={`app ${storyTheme}`}>
+                <StoryComponent />
+            </div>
+        );
+    };
+    HOCStoryComponent.displayName = "ThemeDecorator";
+    return HOCStoryComponent;
 };

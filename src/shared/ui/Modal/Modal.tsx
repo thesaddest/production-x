4a0fb192fb4dd96756bcf9a5b3dc7d8ec1e003ee
thesaddest <artsemshauchuk@gmail.com>
2023-04-20 09:20:27 +0200

    add: init json-server

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -60,7 +60,7 @@ export const Modal: FC<ModalProps> = (props) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, "app_modal"])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={onContentClick}>
                         {children}
